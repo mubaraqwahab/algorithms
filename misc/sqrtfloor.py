495fef9fb35f6e85b07bf4d75ae19e7c731c323c
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-11 21:33:01 +0100

    Add sqrtfloow

---
+++ misc/sqrtfloor.py
@@ -0,0 +1,25 @@
+from math import sqrt, floor
+
+def sqrt_floor(n):
+  """Return the floor of the square root of a positive integer n."""
+
+  candidate = 1
+  for p in range(2, n+1):
+    if p*p > n: break
+    candidate = p
+  return candidate
+
+
+# Test drive
+if __name__ == '__main__':
+  # Test function
+  def test(n):
+    return floor(sqrt(n))
+
+  for i in range(1, 10001):
+    health = sqrt_floor(i) == test(i)
+
+  if health:
+    print("Alhamdulillah, all's well")
+  else:
+    print("Something's wrong")
