e52292ea69a3e6228866c568d995bb023b0c8ee7
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-11 21:20:20 +0100

    Add gcd bruteforce

--- misc/gcd.py
+++ misc/gcd.py
@@ -10,9 +10,23 @@ def gcd_euclid(m, n):
     n = r
   return m
 
+def gcd_bruteforce(m, n):
+  """Compute the greatest common divisor (GCD) of m and n by brute force.
+
+  Params: Two positive integers m and n.
+  Output: The GCD of m and n
+  """
+
+  t = min(m, n)
+  while m % t != 0 or n % t != 0:
+    t -= 1
+  return t
+
+
 # Test drive
 if __name__ == '__main__':
   print("Compute GCD")
   a = int(input("First number? "))
   b = int(input("Second number? "))
   print(f"GCD of {a} and {b} = {gcd_euclid(a, b)}")
+  print(f"GCD of {a} and {b} = {gcd_bruteforce(a, b)}")
