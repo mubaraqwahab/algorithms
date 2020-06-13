a2f816fa64ab408c2b837049072b626bcf931f1d
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-13 23:17:04 +0100

    Complete basic merge sort

--- sorting/index.js
+++ sorting/index.js
@@ -2,11 +2,38 @@ import { selectionSort } from "./selectionsort.js";
 import { bubbleSort } from "./bubblesort.js";
 import { mergesort } from "./mergesort.js";
 
-const arr = [89, 45, 68, 90, 29, 34, 17];
+let arr = [89, 45, 68, 90, 29, 34, 17];
+
 console.log(`Before: ${arr}`);
 
 // selectionSort(arr);
-bubbleSort(arr);
-// mergesort(arr)
+// bubbleSort(arr);
+mergesort(arr);
 
 console.log(`After: ${arr}`);
+
+// TIMING
+
+// let arr = [];
+// for (let i = 0; i < 50; i++) {
+//   arr.push(Math.floor(Math.random() * 100))
+// }
+
+// let arr2 = [ ...arr ];
+// let arr3 = [ ...arr ];
+
+// console.log(`\nBefore: ${arr}\n`);
+
+// console.time("Selection sort")
+// selectionSort(arr);
+// console.timeEnd("Selection sort")
+
+// console.time("bubblesort")
+// bubbleSort(arr2);
+// console.timeEnd("bubblesort")
+
+// console.time("mergesort")
+// mergesort(arr3)
+// console.timeEnd("mergesort")
+
+// console.log(`\nAfter: ${arr}`);
--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -3,5 +3,54 @@
  * @param {Array<number|string>} array
  */
 export function mergesort(array) {
-  // Merge sort!
+  const n = array.length;
+  const fl = Math.floor,
+    ce = Math.ceil;
+
+  if (array.length > 1) {
+    const half1 = array.slice(0, fl(n / 2));
+    const half2 = array.slice(fl(n / 2));
+
+    mergesort(half1);
+    mergesort(half2);
+
+    merge(half1, half2, array);
+  }
+}
+
+/**
+ * Merge two sorted arrays into a third sorted array
+ * @param {Array<number|string>} arrIn1 - a sorted array
+ * @param {Array<number|string>} arrIn2 - another sorted array
+ * @param {Array<number|string>} arrOut - an array to merge arrIn1 and arrIn2 into.
+ */
+function merge(arrIn1, arrIn2, arrOut) {
+  let i = 0,
+    j = 0,
+    k = 0;
+  const p = arrIn1.length,
+    q = arrIn2.length;
+
+  while (i < p && j < q) {
+    if (arrIn1[i] <= arrIn2[j]) {
+      arrOut[k] = arrIn1[i];
+      i++;
+    } else {
+      arrOut[k] = arrIn2[j];
+      j++;
+    }
+    k++;
+  }
+
+  if (i < p) {
+    for (let m = i; m < p; m++) {
+      arrOut[k] = arrIn1[m];
+      k++;
+    }
+  } else {
+    for (let m = i; m < q; m++) {
+      arrOut[k] = arrIn2[m];
+      k++;
+    }
+  }
 }
