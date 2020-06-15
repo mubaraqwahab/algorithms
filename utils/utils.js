b3013596991468364d0a4494ac0c5bd216dae4a4
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-15 13:55:55 +0100

    ...

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,8 @@
  * Return the smaller value of `a` and `b`.
  * @param {any} a
  * @param {any} b
- * @param {function(*): *} [key] `key` should return the key by which `a` and `b` are compared. Ignore this if you want to compare by actual values. (See https://docs.python.org/3/library/stdtypes.html#list.sort)
+ * @param {function(*): *} [key] `key` should return the key by which `a` and `b` are compared.
+ * Ignore this if you want to compare by actual values. (See https://docs.python.org/3/library/stdtypes.html#list.sort)
  */
 export function min(a, b, key = (n) => n) {
   if (key(a) <= key(b)) return a;
@@ -13,7 +14,8 @@ export function min(a, b, key = (n) => n) {
  * Return the larger value of `a` and `b`.
  * @param {any} a
  * @param {any} b
- * @param {function(*): *} [key] - `key` should return the key by which `a` and `b` are compared. Ignore this if you want to compare by actual values.
+ * @param {function(*): *} [key] - `key` should return the key by which `a` and `b` are compared.
+ * Ignore this if you want to compare by actual values.
  */
 export function max(a, b, key = (n) => n) {
   if (key(a) >= key(b)) return a;
@@ -25,7 +27,8 @@ export function max(a, b, key = (n) => n) {
  * @param {any} a
  * @param {any} b
  * @param {any} c
- * @param {function(*): *} [key] `key` should return the key by which `a` and `b` are compared. Ignore this if you want to compare by actual values.
+ * @param {function(*): *} [key] `key` should return the key by which `a` and `b` are compared.
+ * Ignore this if you want to compare by actual values.
  */
 export function medianOfThree(a, b, c, key = (n) => n) {
   const p = key(a),
