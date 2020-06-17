47f56b6b6ff7d73496a0d79c362dd2d11efa80d0
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-17 16:14:16 +0100

    Generalize quicksort

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,30 +1,43 @@
 /**
+
+ */
+
+/**
  * Sort an array by quicksort.
  * @param {array} array - An array to sort.
  * @param {function(T,T): number} compare - A comparison function.
  * When called with any two values `a` and `b` of an array,
  * if it returns a positive number, then `b` precedes `a` in the sorted array.
  * Otherwise `a` precedes `b`.
+ * @param {number} [left]
+ * @param {number} [right]
  */
 export function quicksort(array, compare, left = 0, right = array.length - 1) {
   if (left < right) {
-    const splitPos = partition(array, left, right);
+    const splitPos = partition(array, compare, left, right);
     quicksort(array, compare, left, splitPos - 1);
     quicksort(array, compare, splitPos + 1, right);
   }
 }
 
-function partition(array, left, right) {
+/**
+ * Partition an array by Hoare's algorithm, using the first element as a pivot.
+ * @param {array} array
+ * @param {function(T,T): number} compare
+ * @param {number} left
+ * @param {number} right
+ */
+function partition(array, compare, left, right) {
   const pivot = array[left];
   let i = left,
     j = right + 1;
 
   do {
     do i++;
-    while (array[i] < pivot);
+    while (compare(array[i], pivot) < 0);
 
     do j--;
-    while (array[j] > pivot);
+    while (compare(array[j], pivot) > 0);
 
     if (i < j) {
       // Swap array[i] and array[j]
