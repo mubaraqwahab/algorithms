9396f9d15df9be2c6dfba8819a10673f90267066
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-06-26 22:03:16 +0100

    ...

--- misc/sieve.py
+++ misc/sieve.py
@@ -9,17 +9,18 @@ def sieve(n):
 
   candidates = [ p for p in range(2, n+1) ]
 
-  # for p in range(2, sqrt_floor(n)):
-  #   if candidates[p-2]:
-  #     for j in range(p*p, n+1, p):
-  #       candidates[j-2] = None
-
   p = 2; q = p*p
   while q <= n:
     if candidates[p-2]:
       for j in range(q, n+1, p):
         candidates[j-2] = None
     p += 1; q = p*p
+
+  # OR
+  # for p in range(2, sqrt_floor(n)):
+  #   if candidates[p-2]:
+  #     for j in range(p*p, n+1, p):
+  #       candidates[j-2] = None
 
   return [ r for r in candidates if r ]
 
